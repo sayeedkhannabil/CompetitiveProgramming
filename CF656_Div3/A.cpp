de60f8e816b123af3c3b2fdef345d33b22b6d8bd
sayeedkhannabil <sayeedkhannabil@gmail.com>
2020-07-21 14:57:18 -0500

    A solved

--- CF656_Div3/A.cpp
+++ CF656_Div3/A.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	int testcase;
+	cin >> testcase;
 
+	while (testcase--) {
+		int a[3];
+		
+		for (int i = 0; i < 3; i++) {
+			cin >> a[i];
+		}
+
+		sort(a, a + 3);
+
+		if (a[1] != a[2])
+			cout << "NO\n";
+
+		else
+			cout << "YES\n" << a[0] << " " << a[2] << " " << a[0] << endl;
+
+	}
+
+	return 0;
 }
